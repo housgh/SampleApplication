8a14fb6e8ed9af57b8c2e3174e03e5ab22c6c3f9
Houssam Ghannoum <houssam.ghannoum@elbarid.com>
2022-03-01 16:51:59 +0200

    minor fixes

--- TODO.API/Startup.cs
+++ TODO.API/Startup.cs
@@ -1,28 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Common.Middlewares.ExceptionMiddleware;
 using Common.Utilities;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using TODO.Application;
-using TODO.Application.Abstractions;
-using TODO.Application.Services;
 using TODO.Infrastructure;
-using TODO.Infrastructure.File_Services;
-using TODO.Infrastructure.Notification_Services;
-using TODO.Infrastructure.Third_Party_Services;
 using TODO.Persistence;
-using TODO.Persistence.Caching;
-using TODO.Persistence.Repositories;
 
 namespace TODO.API
 {
@@ -49,10 +35,10 @@ namespace TODO.API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseMiddleware<ExceptionMiddleware>();
 
             if (env.IsDevelopment())
             {
-                app.UseMiddleware<ExceptionMiddleware>();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TODO.API v1"));
             }
